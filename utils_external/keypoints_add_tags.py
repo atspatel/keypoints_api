3d534c168d49d0a390bfe3d921fe6598ed61bf3e
atish <atish@cratconnections.com>
2020-07-04 19:18:06 +0530

    new video content upload

--- utils_external/keypoints_add_tags.py
+++ utils_external/keypoints_add_tags.py
@@ -5,15 +5,20 @@ from accounts.models import User
 # LanguageTag.objects.all().delete()
 # KeypointsCategoryTag.objects.all().delete()
 
-categories = [['Explore', 1], ['Politics', 2], ['World', 3], [
-    'Sports', 4], ['Tech', 5], ['Markets', 6], ['Entertainment', 7]]
+categories = [['Explore', 1, 'https://storage.googleapis.com/kp_videos/app_icons/explore.png'],
+              ['Politics', 2, 'https://storage.googleapis.com/kp_videos/app_icons/politics.png'],
+              ['World', 3, 'https://storage.googleapis.com/kp_videos/app_icons/world.png'],
+              ['Sports', 4, 'https://storage.googleapis.com/kp_videos/app_icons/sports.png'],
+              ['Tech', 5, 'https://storage.googleapis.com/kp_videos/app_icons/tech.png'],
+              ['Markets', 6, 'https://storage.googleapis.com/kp_videos/app_icons/market.png'],
+              ['Entertainment', 7, 'https://storage.googleapis.com/kp_videos/app_icons/entertainment.png']]
 languages = [['English', 1], ['Hindi', 1]]
 
 user_obj = User.objects.filter(phone='8953457318').first()
-for c, i in categories:
+for c, i, thumbnail in categories:
     key = text_to_query(c)
     obj, _ = KeypointsCategoryTag.objects.update_or_create(
-        key=key, defaults={"tag": c, 'index': i, 'created_by': user_obj})
+        key=key, defaults={"tag": c, 'index': i, 'thumbnail_img': thumbnail, 'created_by': user_obj})
 
 for c, i in languages:
     key = text_to_query(c)
---
+++ utils_external/keypoints_add_uploaded_content.py
@@ -0,0 +1,117 @@
+import feedparser
+from django.http import HttpRequest, QueryDict
+
+from accounts.models import User
+from keypoints_account.models import Creator
+from keypoints.models import VideoPost
+from media_ops.models import VideoUrl, ImagesUrl
+from tags_models.models import KeypointsCategoryTag, KeypointsTopicTag
+from tags_models.models import LanguageTag, KeywordsTag
+
+from utils.website_info import get_website_info
+from utils.text_utils import text_to_query
+from utils.video_utils import create_thumbnail_local_video, create_video_hash, create_video_obj_from_file
+
+import pandas as pd
+import random
+import hashlib
+import re
+
+import string
+
+# video_file_name,video_title,user_id,
+# categories, languages,topics,hashtags,ext_url,
+# video_hash, url,hls_url,compressed_url,
+# image_url,image_hash,thumbnail_img
+
+exclude_keys = ["highlites", "mondaymotivation", "thursdayvibes", "tuesdaythoughts",
+                "thursdaymorning", "sundaythoughts", "tuesdaymotivation", "wednesdaywisdom"]
+
+csv_file = "./z_data/video_seed_content.csv"
+df = pd.read_csv(csv_file)
+
+default_user = User.objects.filter(phone='8953457318').first()
+
+for row in df.itertuples():
+    title = row.video_title
+    user_name = row.user_id.strip()
+    categories = row.categories
+    languages = row.languages
+    topics = row.topics
+    hashtags = row.hashtags
+    ext_url = row.ext_url
+
+    creator_obj = Creator.objects.filter(user__first_name=user_name).first()
+    if not creator_obj:
+        print('Creator Not Found ::: ', user_name)
+
+    image_url = row.image_url
+    image_hash = row.image_hash
+    thumbnail_img = row.thumbnail_img
+    image_obj, _ = ImagesUrl.objects.update_or_create(image_hash=image_hash, defaults={
+        "image_url": image_url,
+        "thumbnail_img": thumbnail_img,
+        "media_type": 'image/external',
+        "created_by": default_user})
+
+    video_hash = row.video_hash
+    url = row.url
+    hls_url = row.hls_url
+    compressed_url = row.compressed_url
+    thumbnail_img = image_obj.thumbnail_img
+
+    video_obj, _ = VideoUrl.objects.update_or_create(video_hash=video_hash, defaults={
+        "url": url,
+        "thumbnail_img": thumbnail_img,
+        "hls_url": hls_url,
+        "compressed_url": compressed_url
+    })
+
+    post_obj, _ = VideoPost.objects.update_or_create(
+        video=video_obj, defaults={
+            "creator": creator_obj,
+            "title": title,
+            "external_urls": ext_url}
+    )
+    categories = [] if pd.isnull(categories) else categories.split(', ')
+    for category in categories:
+        if(category == "General"):
+            category = "Explore"
+        key = re.sub("[^a-z]+", "", category.lower())
+        category_obj = KeypointsCategoryTag.objects.filter(key=key).first()
+        if category_obj:
+            post_obj.categories.add(category_obj)
+        else:
+            print("Category not found :: ", category)
+
+    languages = [] if pd.isnull(languages) else languages.split(', ')
+    for language in languages:
+        key = re.sub("[^a-z]+", "", language.lower())
+        language_obj = LanguageTag.objects.filter(key=key).first()
+        if language_obj:
+            post_obj.languages.add(language_obj)
+        else:
+            print("language not found :: ", language)
+
+    topics = [] if pd.isnull(topics) else topics.split(', ')
+    for topic in topics:
+        key = text_to_query(topic)
+        if not key:
+            continue
+        topic_obj, _ = KeypointsTopicTag.objects.get_or_create(
+            key=key, defaults={'tag': topic})
+        post_obj.topics.add(topic_obj)
+
+    hashtags = [] if pd.isnull(hashtags) else hashtags.split('#')
+    for hashtag in hashtags:
+        hashtag = hashtag.strip()
+        key = text_to_query(hashtag)
+        if not key:
+            continue
+        if key in exclude_keys:
+            continue
+        keywords_obj, _ = KeywordsTag.objects.get_or_create(
+            key=key, defaults={'tag': hashtag})
+        post_obj.hashtags.add(keywords_obj)
+
+    post_obj.save()
