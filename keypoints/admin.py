340bd83912afe5ae52d99652f6a002b0e4dae785
atish <atish@cratconnections.com>
2020-07-27 08:58:01 +0530

    video post admin

--- keypoints/admin.py
+++ keypoints/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.apps import apps
 
+from .views import VideoPost
+
 # Register your models here.
+
+
+class VideoPostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'creator', 'title',
+                    'creation_date', 'update_date']
+
+
+admin.site.register(VideoPost, VideoPostAdmin)
+
 app = apps.get_app_config('keypoints')
 for model_name, model in app.models.items():
-    admin.site.register(model)
+    if model_name not in ["VideoPost".lower()]:
+        admin.site.register(model)
