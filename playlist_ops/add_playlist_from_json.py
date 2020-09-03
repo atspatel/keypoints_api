4e698a20546bcc222dd565f7e139bea98bb2e0c8
atish <atish@cratconnections.com>
2020-09-04 00:51:58 +0530

    json folder to db

--- playlist_ops/add_playlist_from_json.py
+++ playlist_ops/add_playlist_from_json.py
@@ -1,3 +1,4 @@
+import os
 import json
 import hashlib
 
@@ -11,53 +12,57 @@ from playlist_ops.models import PlaylistInfo, PlaylistMediaMapping
 admin_user = User.objects.filter(phone='8953457318').first()
 
 
-json_file_path = "./z_data/playlist.json"
+json_folder = "./z_data/json_folder/"
+for f in sorted(os.listdir(json_folder)):
+    if not f.endswith(".json"):
+        continue
+    json_file_path = os.path.join(json_folder, f)
 
-data = json.load(open(json_file_path, 'r'))
+    data = json.load(open(json_file_path, 'r'))
 
-video_source = "ShareChat"
+    video_source = "ShareChat"
 
-parent_title_obj = None
-parent_lang = None
+    parent_title_obj = None
+    parent_lang = None
 
-title_obj = get_title_obj(data.get('title', {}), created_by=admin_user)
-lang_obj = get_lang_obj(data.get('language', "Hindi"))
+    title_obj = get_title_obj(data.get('title', {}), created_by=admin_user)
+    lang_obj = get_lang_obj(data.get('language', "Hindi"))
 
-videoId = data.get('videoId', None)
-if videoId:
-    primary = data.get('primary', 'video')
-    primaryList = data.get('primaryList', [])
+    videoId = data.get('videoId', None)
+    if videoId:
+        primary = data.get('primary', 'video')
+        primaryList = data.get('primaryList', [])
 
-    secondary = data.get('secondary', None)
+        secondary = data.get('secondary', None)
 
-    isSingleSecondary = False
-    secondaryList = []
-    if secondary:
-        isSingleSecondary = data.get('isSingleSecondary', False)
-        secondaryList = data.get('secondaryList', [])
-        if isSingleSecondary and len(secondaryList) > 1:
-            secondaryList = secondaryList[:1]
+        isSingleSecondary = False
+        secondaryList = []
+        if secondary:
+            isSingleSecondary = data.get('isSingleSecondary', False)
+            secondaryList = data.get('secondaryList', [])
+            if isSingleSecondary and len(secondaryList) > 1:
+                secondaryList = secondaryList[:1]
 
-    playlist_obj, _ = PlaylistInfo.objects.update_or_create(name=videoId, defaults={
-        "language": lang_obj,
-        "title": title_obj,
-        "primary": primary.lower(),
-        "secondary": secondary.lower() if secondary else None,
-        "isSingleSecondary": isSingleSecondary,
-        "created_by": admin_user
-    })
+        playlist_obj, _ = PlaylistInfo.objects.update_or_create(name=videoId, defaults={
+            "language": lang_obj,
+            "title": title_obj,
+            "primary": primary.lower(),
+            "secondary": secondary.lower() if secondary else None,
+            "isSingleSecondary": isSingleSecondary,
+            "created_by": admin_user
+        })
 
-    for index, media in enumerate(primaryList):
-        media_obj = get_media_obj(media, created_by=admin_user)
-        if media_obj:
-            mapping_obj = PlaylistMediaMapping.objects.update_or_create(
-                playlist=playlist_obj, media=media_obj, defaults={'media_category': PRIMARY, "index": index})
-
-    if secondary:
-        for index, media in enumerate(secondaryList):
+        for index, media in enumerate(primaryList):
             media_obj = get_media_obj(media, created_by=admin_user)
             if media_obj:
                 mapping_obj = PlaylistMediaMapping.objects.update_or_create(
-                    playlist=playlist_obj, media=media_obj, defaults={'media_category': SECONDARY, "index": index})
+                    playlist=playlist_obj, media=media_obj, defaults={'media_category': PRIMARY, "index": index})
 
-    print(playlist_obj.id)
+        if secondary:
+            for index, media in enumerate(secondaryList):
+                media_obj = get_media_obj(media, created_by=admin_user)
+                if media_obj:
+                    mapping_obj = PlaylistMediaMapping.objects.update_or_create(
+                        playlist=playlist_obj, media=media_obj, defaults={'media_category': SECONDARY, "index": index})
+
+        print(f, playlist_obj.id)
