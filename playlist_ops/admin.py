393a1367e32ff66e761209cec5f4f6b9036e005e
Aatish Patel <atish@cratconnections.com>
2020-12-02 13:44:57 +0530

    admin for playlist_ops

--- playlist_ops/admin.py
+++ playlist_ops/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.apps import apps
 
+from .models import PlaylistInfo
+
+
+class PlaylistInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+admin.site.register(PlaylistInfo, PlaylistInfoAdmin)
+
+
 app = apps.get_app_config('playlist_ops')
 for model_name, model in app.models.items():
-    admin.site.register(model)
+    if model_name not in ["PlaylistInfo".lower()]:
+        admin.site.register(model)
